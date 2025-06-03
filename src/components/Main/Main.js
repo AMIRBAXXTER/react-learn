97af0118ac8b878b6f0586396e31cd2b508fc491
AMIRBAXXTER <amirmasoodpormahdi@gmail.com>
2025-06-03 14:15:08 +0330

    some changes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,22 +1,69 @@
 import User from "./User/User";
-export default function Main() {
-	const users = [
-		{ id: 1, age: 27, image: "user_images/1.webp" },
-		{ id: 7, name: "reza", age: 22, image: "user_images/2.webp" },
-		{ id: 10, name: "ahmad", age: 18, image: "user_images/3.jfif" },
-		{ id: 11, name: "amir", age: 30 },
-		{ id: 12, name: "zahra", age: 25 },
-	];
-	return (
-		<>
-			<div className="flex flex-wrap justify-center px-7 gap-4 bg-[#969696] w-[70%] shadow-2xl">
-				{users.map((user) => (
-					<User {...user} key={user.id} />
-				))}
-			</div>
-			<div className="w-1/4 bg-orange-100 justify-items-center h-10 mt-10">
-				<h1>test</h1>
-			</div>
-		</>
-	);
+import React, { PureComponent } from "react";
+import { BeatLoader } from "react-spinners";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default class Main extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			randomUsers: [],
+		};
+	}
+	addRandomUser = async () => {
+		await fetch("https://randomuser.me/api/")
+			.then((res) => res.json())
+			.then((data) =>
+				this.setState((pervState) => ({
+					randomUsers: [...pervState.randomUsers, data.results[0]],
+				}))
+			)
+			.catch((err) => console.log(err));
+	};
+	createRandomUserList = async (number) => {
+		await fetch(`https://randomuser.me/api/?results=${number}`)
+			.then((res) => res.json())
+			.then((data) => this.setState({ randomUsers: data.results }))
+			.catch((err) => console.log(err));
+	};
+
+	componentDidMount() {
+		this.createRandomUserList(8);
+	}
+
+	render() {
+		return (
+			<>
+				<div className="flex flex-wrap justify-center px-7 gap-4 bg-slate-200 w-[70%] shadow-2xl rounded-xl">
+					{this.state.randomUsers.length === 0 ? (
+						<BeatLoader className="h-40 mt-36" color={"#36d7b7"} />
+					) : (
+						<AnimatePresence>
+							{this.state.randomUsers.map((user, i) => (
+								<motion.div
+									key={`random-${i}`}
+									initial={{ opacity: 0, y: 40 }}
+									animate={{ opacity: 1, y: 0 }}
+									exit={{ opacity: 0, y: 40 }}
+									transition={{ duration: 0.4, delay: i * 0.07 }}
+								>
+									<User
+										name={user.name?.first}
+										age={user.dob?.age}
+										image={user.picture?.large}
+									/>
+								</motion.div>
+							))}
+						</AnimatePresence>
+					)}
+				</div>
+				<button
+					onClick={this.addRandomUser}
+					className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-10"
+				>
+					add new User
+				</button>
+			</>
+		);
+	}
 }
--- src/components/Main/User/User.js
+++ src/components/Main/User/User.js
@@ -10,8 +10,8 @@ export default function User({
   return (
       <div className="my-7">
         <LuSquareUserRound size={20} color={"rgba(255, 0, 174, 0.77)"}/>
-        <li className="">{Username}</li>
-        <li>{age}</li>
+        <li className="list-none m-auto w-full text-center">{Username}</li>
+        <li className="list-none m-auto w-full text-center">{age}</li>
         <img alt={Username} src={image} className="w-52 h-52 hover:rotate-2 hover:scale-105 hover:opacity-80 rounded-md transition-all" />
       </div> 
     )
